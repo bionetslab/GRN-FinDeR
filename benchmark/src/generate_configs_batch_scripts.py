e0d80cb635f4651fc51b0ccb2216f542613ece95
Anne Hartebrodt <anne.hartebrodt@fau.de>
2025-02-07 12:59:25 +0100

    jobscript generator

--- benchmark/src/generate_configs_batch_scripts.py
+++ benchmark/src/generate_configs_batch_scripts.py
@@ -60,6 +60,45 @@ conda deactivate
         with open(jobscript_file, 'w') as handle:
             handle.write(script_content)
 
+def save_preprocessing_script(config, jobscript_path):
+
+    '''
+    Save the Slurm batch scripts.
+    '''
+    tissues = ['Blood', 'Brain', 'Adipose Tissue', 'Muscle', 'Blood Vessel', 'Heart', 'Ovary', 'Uterus', 'Vagina',
+            'Breast', 'Skin', 'Salivary Gland', 'Adrenal Gland', 'Thyroid', 'Lung', 'Spleen',
+            'Pancreas', 'Esophagus', 'Stomach', 'Colon', 'Small Intestine', 'Prostate', 'Testis', 'Nerve',
+            'Pituitary', 'Liver', 'Kidney', 'Cervix Uteri', 'Fallopian Tube', 'Bladder', 'Bone Marrow']
+
+    for t in tissues:
+        config['tissue'] = t
+        name = t.replace(' ', '_')
+        jobscript_file = op.join(jobscript_path, f'{name}_script.sh')
+
+        script_content = f"""#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time=00:15:00
+#SBATCH --export=NONE
+unset SLURM_EXPORT_ENV
+# set number of threads to requested cpus-per-task
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+# for Slurm version >22.05: cpus-per-task has to be set again for srun
+export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
+echo $SLURM_CPUS_PER_TASK
+echo $SRUN_CPUS_PER_TASK
+module load python
+conda activate grn-finder 
+cd $WORK 
+srun python GRN-FinDeR/benchmark/src/preprocess_gtex.py -f GRN-FinDeR/benchmark/configs/{t}.yaml
+cp $TMPDIR/{t} {config['results_dir']}/{t}
+conda deactivate
+"""
+
+        with open(jobscript_file, 'w') as handle:
+            handle.write(script_content)
+
 
 if __name__ == '__main__':
     import argparse
@@ -68,15 +107,23 @@ if __name__ == '__main__':
     # Add the file argument
     parser.add_argument('-f', type=str, help='The file to process')
     parser.add_argument('-o', type=str, help='The output_directory')
-    parser.add_argument('-j', type=str, help='Batch submissions scripts')
+    parser.add_argument('-j', type=str, help='Batch submissions scripts', default=None)
+    parser.add_argument('-p', type=str, help='Batch preproccessing scripts', default=None)
 
     # Parse the arguments
     args = parser.parse_args()
     config = read_template(args.f)
     os.makedirs(args.o, exist_ok=True)
     save_new_configs(args.o, config)
-    os.makedirs(args.j, exist_ok=True)
-    save_jobscript(config, args.j)
+
+    if args.j is not None:
+        os.makedirs(args.j, exist_ok=True)
+        save_jobscript(config, args.j)
+    if args.p is not None:
+        os.makedirs(args.p, exist_ok=True)
+        save_preprocessing_script(config, args.p)
+
+
 
 
 
