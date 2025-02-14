078e1bf5a5d38cf6524d6ca6fd94926d255c72a5
Paul Martini <paul.martini@web.de>
2025-02-14 14:46:47 +0100

    add median distance and silhouette plots, remove main

--- src/postprocessing.py
+++ src/postprocessing.py
@@ -4,7 +4,7 @@ import os
 import matplotlib.pyplot as plt
 
 
-def plot_cluster_metrics(file_path : str, clustering_sizes : list[int]):
+def plot_cluster_metrics(file_path : str, num_clusters : list[int]):
     """Plot different precomputed characteristics of clusters for all GTEX tissues.
 
     Args:
@@ -16,26 +16,36 @@ def plot_cluster_metrics(file_path : str, clustering_sizes : list[int]):
     # Process all tissues.
     for tissue, subdir in zip(tissues, subdirectories):
         print(f'Processing tissue {tissue}...')
-        
+        subdir = os.path.join(subdir, 'clustering_metrics')
         # Open precompute cluster metrics.
         with open(os.path.join(subdir, 'diameters_per_clustering.pkl'), 'rb') as f:
             diams_per_cluster_dict = pickle.load(f)
         
+        with open(os.path.join(subdir, 'median_distances_per_clustering.pkl'), 'rb') as f:
+            median_distances_per_clustering = pickle.load(f)
+
         with open(os.path.join(subdir, 'num_singletons_per_clustering.pkl'), 'rb') as f:
             singletons_per_clustering = pickle.load(f)
             
         with open(os.path.join(subdir, 'sizes_per_clustering.pkl'), 'rb') as f:
             sizes_per_clustering = pickle.load(f)
+
+        with open(os.path.join(subdir, 'silhouette_score_per_clustering.pkl'), 'rb') as f:
+            silhouette_score_per_clustering = pickle.load(f)
             
-        subset_diams_per_clustering = {key : val for key,val in diams_per_cluster_dict.items() if key in clustering_sizes}
-        subset_singletons_per_clustering = {key : val for key,val in singletons_per_clustering.items() if key in clustering_sizes}
-        subset_sizes_per_clustering = {key : val for key, val in sizes_per_clustering.items() if key in clustering_sizes}
+        subset_diams_per_clustering = {key : val for key,val in diams_per_cluster_dict.items() if key in num_clusters}
+        subset_median_distances_per_clustering = {key: val for key, val in median_distances_per_clustering.items() if key in num_clusters}
+        subset_singletons_per_clustering = {key : val for key,val in singletons_per_clustering.items() if key in num_clusters}
+        subset_sizes_per_clustering = {key : val for key, val in sizes_per_clustering.items() if key in num_clusters}
+        subset_silhouette_score_per_clustering = {key: val for key, val in silhouette_score_per_clustering.items() if key in num_clusters}
         
         # Plot diameters, singletons, and sizes.
-        x_labels = clustering_sizes
+        x_labels = num_clusters
         diams_data = list(subset_diams_per_clustering.values())
+        median_dist_data = list(subset_median_distances_per_clustering.values())
         sizes_data = list(subset_sizes_per_clustering.values())
         singletons_list = [val for _, val in subset_singletons_per_clustering.items()]
+        silhouette_list = [val for _, val in subset_silhouette_score_per_clustering.items()]
 
         dpi = 300
         fig, ax = plt.subplots(figsize=(8, 5), dpi=dpi)
@@ -56,14 +66,30 @@ def plot_cluster_metrics(file_path : str, clustering_sizes : list[int]):
         plt.close(fig)
 
         fig, ax = plt.subplots(figsize=(8, 5), dpi=dpi)
+        ax.violinplot(median_dist_data)
+        ax.set_xticks(list(range(1, len(median_dist_data) + 1)))
+        ax.set_xticklabels(x_labels)
+        ax.set_xlabel("Number of Clusters")
+        ax.set_ylabel("Cluster Median Distance")
+        plt.savefig(os.path.join(subdir, 'cluster_median_distance.png'), dpi=dpi)
+        plt.close(fig)
+
+        fig, ax = plt.subplots(figsize=(8, 5), dpi=dpi)
         ax.plot(x_labels, singletons_list, marker='o')
         # ax.set_xticks(list(range(1, len(diams_data) + 1)))
         # ax.set_xticklabels(x_labels)
         ax.set_xlabel("Number of Clusters")
         ax.set_ylabel("Number of Singletons")
         plt.savefig(os.path.join(subdir, 'number_of_singletons.png'), dpi=dpi)
-        
-        
-if __name__ == "__main__":
-    plot_cluster_metrics('/data/bionets/ac07izid/grn/data/gtex_cluster_data', list(range(100, 5001, 100)))
+        plt.close(fig)
+
+        fig, ax = plt.subplots(figsize=(8, 5), dpi=dpi)
+        ax.plot(x_labels, silhouette_list, marker='o')
+        # ax.set_xticks(list(range(1, len(diams_data) + 1)))
+        # ax.set_xticklabels(x_labels)
+        ax.set_xlabel("Number of Clusters")
+        ax.set_ylabel("Silhouette Score")
+        plt.savefig(os.path.join(subdir, 'silhouette_score.png'), dpi=dpi)
+        plt.close(fig)
+
     
