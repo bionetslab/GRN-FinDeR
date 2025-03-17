6ba55c60ef87368652c3b78a798f56db8da7a24a
Paul Martini <paul.martini@web.de>
2025-03-17 13:11:11 +0100

    misc

--- src/fdr_calculation.py
+++ src/fdr_calculation.py
@@ -13,14 +13,6 @@ from src.preprocessing import preprocess_data
 import random
 import itertools
 
-def _invert_gene_cluster_dictionary(gene_to_cluster : dict):
-    cluster_to_gene = dict()
-    for key, val in gene_to_cluster.items():
-        if val in cluster_to_gene:
-            cluster_to_gene[val].append(key)
-        else:
-            cluster_to_gene[val] = [key]
-    return cluster_to_gene
 
 def approximate_fdr(expression_mat : pd.DataFrame,
                     grn : pd.DataFrame,
@@ -40,88 +32,25 @@ def approximate_fdr(expression_mat : pd.DataFrame,
                                             num_permutations)
     return fdr_grn
 
-def merge_clusterings(tf_to_cluster : dict, gene_to_cluster : dict):
-    tf_to_cluster = tf_to_cluster.copy()
-    num_tf_categories = max(tf_to_cluster.values())+1
-    gene_to_cluster_updated = {gene : cluster+num_tf_categories for gene, cluster in gene_to_cluster.items()}
-    tf_to_cluster.update(gene_to_cluster_updated)
-    return tf_to_cluster
-
-def _approximate_fdr_with_tfs(expression_mat : pd.DataFrame,
-                              grn : pd.DataFrame,
-                              tf_to_cluster : dict,
-                              gene_to_cluster : dict,
-                              num_permutations : int = 1000):
-    target_to_cluster = merge_clusterings(tf_to_cluster, gene_to_cluster)
-    cluster_to_target = _invert_gene_cluster_dictionary(target_to_cluster)
-    cluster_to_gene = _invert_gene_cluster_dictionary(gene_to_cluster)
-    cluster_to_tf = _invert_gene_cluster_dictionary(tf_to_cluster)
-    
-    # Create dict representation from CSV input.
-    grn_zipped = zip(grn['TF'].to_list(), grn['target'].to_list(), grn['importance'].to_list())
-    grn_dict = {(tf, target): (importance, 0.0, 0.0) for tf, target, importance in grn_zipped}
-    
-    # Edge count structure on cluster level with structure {(TFclusterID, TargetclusterID) : count}.
-    cluster_cluster_counts = {
-        cluster_edge : 0.0 for cluster_edge in itertools.product(
-            list(cluster_to_tf.keys()),
-            list(cluster_to_target.keys())
-            )
-        }
-    
-    for i in range(num_permutations):
-        # Sample representatives from each cluster.
-        tf_representatives = _draw_representatives(cluster_to_tf, 1)
-        gene_representatives = _draw_representatives(cluster_to_gene, 1)
-        
-        joint_representatives = list(set(tf_representatives + gene_representatives))
-        represent_expression = expression_mat[joint_representatives].copy()
-        # Shuffle expression matrix.
-        represent_permuted = represent_expression.sample(frac=1, axis=1)
-        shuffled_grn = grnboost2(expression_data=represent_permuted, 
-                                 tf_names=tf_representatives)
-        # Compute adjusted count values for each shuffled edge.
-        for tf, target, factor in zip(shuffled_grn['TF'], shuffled_grn['target'], shuffled_grn['importance']):
-            if (tf, target) in grn_dict:
-                count_value = int(factor >= grn_dict[(tf, target)][0])
-                # Update corresponding cluster counts.
-                cluster_cluster_counts[(tf_to_cluster[tf], target_to_cluster[target])] += count_value
-
-    # Map cluster-cluster Pvalues into original genes.
-    for key in grn_dict.keys():
-        cluster_tuple = (tf_to_cluster[key[0]], target_to_cluster[key[1]])
-        p_value = (cluster_cluster_counts[cluster_tuple]+1) / (num_permutations+1)
-        grn_dict[key] = (grn_dict[key][0], cluster_cluster_counts[cluster_tuple], p_value)
-    
-    grn_transposed = {'tf': [], 'target' : [], 'importance' : [], 'count': [], 'pvalue': []}
-    for key, val in grn_dict.items():
-        grn_transposed['tf'].append(key[0])
-        grn_transposed['target'].append(key[1])
-        grn_transposed['importance'].append(val[0])
-        grn_transposed['count'].append(val[1])
-        grn_transposed['pvalue'].append(val[2])
-    
-    grn_df = pd.DataFrame.from_dict(grn_transposed)
-    return grn_df
 
 def _approximate_fdr_no_tfs(
-                    expression_mat : pd.DataFrame,
-                    grn : pd.DataFrame,
-                    gene_to_cluster : dict,
-                    num_permutations : int = 1000):
-    
+        expression_mat: pd.DataFrame,
+        grn: pd.DataFrame,
+        gene_to_cluster: dict,
+        num_permutations: int = 1000):
+
     # Invert gene to cluster dictionary.
     cluster_to_gene = _invert_gene_cluster_dictionary(gene_to_cluster)
 
     # Compute set of singleton genes, i.e. genes that are in one-element-cluster.
-    singleton_genes = {genes[0] for _, genes in cluster_to_gene.items() if len(genes)==1}
+    singleton_genes = {genes[0] for _, genes in cluster_to_gene.items() if len(genes) == 1}
 
     # Create dict from original GRN {('TF', 'target'): ('importance', 'counter')}
     grn_zipped = zip(grn['TF'].to_list(), grn['target'].to_list(), grn['importance'].to_list())
     grn_dict = {(tf, target): (importance, 0.0, 0.0) for tf, target, importance in grn_zipped}
-    
+
     # Init data structure for counting between and withing cluster empirical counts.
-    cluster_cluster_counts = {cluster_edge : 0.0 for cluster_edge in itertools.product(cluster_to_gene.keys(), repeat=2)}
+    cluster_cluster_counts = {cluster_edge: 0.0 for cluster_edge in itertools.product(cluster_to_gene.keys(), repeat=2)}
 
     for i in range(num_permutations):
         # Sample representatives from each cluster.
@@ -129,7 +58,7 @@ def _approximate_fdr_no_tfs(
         represent_expression = expression_mat[representatives].copy()
         # Shuffle expression matrix.
         represent_permuted = represent_expression.sample(frac=1, axis=1)
-        shuffled_grn = grnboost2(expression_data=represent_permuted, 
+        shuffled_grn = grnboost2(expression_data=represent_permuted,
                                  tf_names=represent_permuted.columns.to_list())
         # Compute adjusted count values for each shuffled edge.
         for tf, target, factor in zip(shuffled_grn['TF'], shuffled_grn['target'], shuffled_grn['importance']):
@@ -153,23 +82,99 @@ def _approximate_fdr_no_tfs(
                     count_value /= 4.0
                 # Update corresponding cluster counts.
                 cluster_cluster_counts[(gene_to_cluster[tf], gene_to_cluster[target])] += count_value
-    
+
     # Write cluster-cluster Pvalues into original genes.
     for key in grn_dict.keys():
         cluster_tuple = (gene_to_cluster[key[0]], gene_to_cluster[key[1]])
-        p_value = (cluster_cluster_counts[cluster_tuple]+1) / (num_permutations+1)
+        p_value = (cluster_cluster_counts[cluster_tuple] + 1) / (num_permutations + 1)
         grn_dict[key] = (grn_dict[key][0], cluster_cluster_counts[cluster_tuple], p_value)
-    
-    grn_transposed = {'tf': [], 'target' : [], 'importance' : [], 'count': [], 'pvalue': []}
+
+    grn_transposed = {'tf': [], 'target': [], 'importance': [], 'count': [], 'pvalue': []}
     for key, val in grn_dict.items():
         grn_transposed['tf'].append(key[0])
         grn_transposed['target'].append(key[1])
         grn_transposed['importance'].append(val[0])
         grn_transposed['count'].append(val[1])
         grn_transposed['pvalue'].append(val[2])
-    
+
     grn_df = pd.DataFrame.from_dict(grn_transposed)
     return grn_df
+
+
+def _approximate_fdr_with_tfs(expression_mat: pd.DataFrame,
+                              grn: pd.DataFrame,
+                              tf_to_cluster: dict,
+                              gene_to_cluster: dict,
+                              num_permutations: int = 1000):
+    target_to_cluster = _merge_clusterings(tf_to_cluster, gene_to_cluster)
+    cluster_to_target = _invert_gene_cluster_dictionary(target_to_cluster)
+    cluster_to_gene = _invert_gene_cluster_dictionary(gene_to_cluster)
+    cluster_to_tf = _invert_gene_cluster_dictionary(tf_to_cluster)
+
+    # Create dict representation from CSV input.
+    grn_zipped = zip(grn['TF'].to_list(), grn['target'].to_list(), grn['importance'].to_list())
+    grn_dict = {(tf, target): (importance, 0.0, 0.0) for tf, target, importance in grn_zipped}
+
+    # Edge count structure on cluster level with structure {(TFclusterID, TargetclusterID) : count}.
+    cluster_cluster_counts = {
+        cluster_edge: 0.0 for cluster_edge in itertools.product(
+            list(cluster_to_tf.keys()),
+            list(cluster_to_target.keys())
+        )
+    }
+
+    for i in range(num_permutations):
+        # Sample representatives from each cluster.
+        tf_representatives = _draw_representatives(cluster_to_tf, 1)
+        gene_representatives = _draw_representatives(cluster_to_gene, 1)
+
+        joint_representatives = list(set(tf_representatives + gene_representatives))
+        represent_expression = expression_mat[joint_representatives].copy()
+        # Shuffle expression matrix.
+        represent_permuted = represent_expression.sample(frac=1, axis=1)
+        shuffled_grn = grnboost2(expression_data=represent_permuted,
+                                 tf_names=tf_representatives)
+        # Compute adjusted count values for each shuffled edge.
+        for tf, target, factor in zip(shuffled_grn['TF'], shuffled_grn['target'], shuffled_grn['importance']):
+            if (tf, target) in grn_dict:
+                count_value = int(factor >= grn_dict[(tf, target)][0])
+                # Update corresponding cluster counts.
+                cluster_cluster_counts[(tf_to_cluster[tf], target_to_cluster[target])] += count_value
+
+    # Map cluster-cluster Pvalues into original genes.
+    for key in grn_dict.keys():
+        cluster_tuple = (tf_to_cluster[key[0]], target_to_cluster[key[1]])
+        p_value = (cluster_cluster_counts[cluster_tuple] + 1) / (num_permutations + 1)
+        grn_dict[key] = (grn_dict[key][0], cluster_cluster_counts[cluster_tuple], p_value)
+
+    grn_transposed = {'tf': [], 'target': [], 'importance': [], 'count': [], 'pvalue': []}
+    for key, val in grn_dict.items():
+        grn_transposed['tf'].append(key[0])
+        grn_transposed['target'].append(key[1])
+        grn_transposed['importance'].append(val[0])
+        grn_transposed['count'].append(val[1])
+        grn_transposed['pvalue'].append(val[2])
+
+    grn_df = pd.DataFrame.from_dict(grn_transposed)
+    return grn_df
+
+
+def _invert_gene_cluster_dictionary(gene_to_cluster : dict):
+    cluster_to_gene = dict()
+    for key, val in gene_to_cluster.items():
+        if val in cluster_to_gene:
+            cluster_to_gene[val].append(key)
+        else:
+            cluster_to_gene[val] = [key]
+    return cluster_to_gene
+
+
+def _merge_clusterings(tf_to_cluster : dict, gene_to_cluster : dict):
+    tf_to_cluster = tf_to_cluster.copy()
+    num_tf_categories = max(tf_to_cluster.values())+1
+    gene_to_cluster_updated = {gene : cluster+num_tf_categories for gene, cluster in gene_to_cluster.items()}
+    tf_to_cluster.update(gene_to_cluster_updated)
+    return tf_to_cluster
 
 
 def _draw_representatives(cluster_to_gene : dict, num_representatives : int = 2):
